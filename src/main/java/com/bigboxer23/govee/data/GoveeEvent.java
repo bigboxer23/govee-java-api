2f0fb607f7e19d50dc5a470e9cf317fafa1346a0
Matt Jones <bigboxer23@gmail.com>
2024-03-19 12:35:48 -0500

    feat: map data model to names that make more sense. (#7)

--- src/main/java/com/bigboxer23/govee/data/GoveeEvent.java
+++ src/main/java/com/bigboxer23/govee/data/GoveeEvent.java
@@ -1,14 +1,18 @@
 package com.bigboxer23.govee.data;
 
 import java.util.List;
+
+import com.squareup.moshi.Json;
 import lombok.Data;
 
 /** */
 @Data
 public class GoveeEvent {
-	private String sku;
+	@Json(name = "sku")
+	private String model;
 
-	private String device;
+	@Json(name = "device")
+	private String deviceId;
 
 	private String deviceName;
 
